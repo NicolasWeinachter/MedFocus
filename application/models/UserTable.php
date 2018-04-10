4458a01de746d40c45e0fe9011e18dc17aba1210
Raphaël <raphaelamyco@gmail.com>
2018-04-10 17:39:54 +0200

    update_user

--- application/models/UserTable.php
+++ application/models/UserTable.php
@@ -43,6 +43,33 @@ class UserTable extends CI_Model
                         ->delete($this->table);
     }
 
+    /**
+     * Modifie les informations d'un utilisateur
+     * 
+     *	@param string $email
+	 *	@param string $pwd
+     *	@param string $name
+     *	@param string $surname
+     *	@param string $gender
+     *	@param string $bday
+     *	@param int $num_tel
+     *	@param int $num_secu
+     *  @return bool le résultat de la requête
+     */
+    public function update_user($email, $pwd, $name, $surname, $gender, $bday, $num_tel, $num_secu)
+{  
+        $data = array (
+            'password'  => $pwd,
+            'name'  => $name,
+            'surname'  => $surname,
+            'gender'  => $gender,
+            'bday'  => $bday,
+            'num_tel'  => $num_tel,
+            'num_secu'  => $num_secu,
+        );
+        $this->db->where('email', $email);
+        return $this->db->update('users', $data); 
+    }
 
     /**
      *	Retourne une liste de $nb dernières news.
