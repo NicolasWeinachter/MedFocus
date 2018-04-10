288d8d746d0872e0e8978f7829eb8192a4f1bbed
NicolasWeinachter <31858659+NicolasWeinachter@users.noreply.github.com>
2018-04-10 17:43:03 +0200

    Update head + gitignore

--- application/views/home/homepage.php
+++ application/views/home/homepage.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" >
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url("assets/css/medfocus.css"); ?>" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/medfocus.css"); ?>" >
 	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-3.3.1.min.js"); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
     <title>MedFocus</title>
 </head>
-
 
 <body>
 	<div id="container1">
@@ -73,4 +72,43 @@
 		
 	</div>
 
+	<footer>
+		<div id="containerFin">
+		<img class="logo-fin" src="<?php echo base_url('/assets/images/website/Logo-03.png'); ?>" width=200px>
+		<table class="table">
+			<thead>
+				<tr>
+					
+					<th scope="col">MedFocus</th>
+					<th scope="col">Mentions légales</th>
+					<th scope="col">CGU</th>
+					<th scope="col">Professionnel de santé ?</th>
+				</tr>
+			</thead>
+ 			<tbody>
+ 				<tr>
+				    <td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/about"); ?>">A propos de nous<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/faq"); ?>">Questions fréquentes<br></a>
+                        <a class =" lien-normal text-blanc" href="<?php echo site_url("home/recrutement"); ?>">Recrutement<br></a>
+                        <a class =" lien-normal text-blanc" href="<?php echo site_url("home/contact"); ?>">Contact</a>
+					</td>
+					<td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/legal"); ?>">La législation<br></a>
+					</td>
+					<td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/cgu"); ?>">Conditions générales d'utilisation<br></a>
+      				</td> 
+      				<td>
+                        <a class =" lien-normal text-blanc text-blanc" href ="<?php echo site_url("home/services"); ?>">Nos services<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/tarifs"); ?>">Nos tarifs<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/eds"); ?>">Etablissements de santé<br></a>
+      				</td>
+   				</tr>
+  			</tbody>
+		</table>
+	</div>
+	</footer>
+
 </body>
+</html>
