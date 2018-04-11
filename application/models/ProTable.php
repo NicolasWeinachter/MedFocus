78bbb653216bf9b9a8d8b2411db301c5339a3e9a
Raphaël <raphaelamyco@gmail.com>
2018-04-11 10:08:21 +0200

    update_user

--- application/models/ProTable.php
+++ application/models/ProTable.php
@@ -66,5 +66,56 @@ class ProTable extends CI_Model
                         ->delete($this->table);
     }
 
+    /**
+     * Modifie les informations d'un professionnel
+     * 
+     *	@param string $email
+	 *	@param string $pwd
+     *	@param string $name
+     *	@param string $surname
+     *	@param string $bday
+     *	@param string $gender
+     *	@param string $job
+     *  @param string $spe
+     *  @param string $exp
+     *  @param int $num_RPPS
+     *  @param bool $aga
+     *  @param string $payment
+     *  @param bool $carte_vitale
+     *  @param string $address
+     *  @param string $postcode
+     *  @param string $city
+     *  @param string $practical
+     *  @param int $num_tel
+     *  @param bool $premium
+	 *	@return bool Le résultat de la requête
+     */
+    public function update_user($email, $pwd, $name, $surname, $bday, $gender, $job, $spe, $exp, $num_RPPS, $aga, $payment,
+    $carte_vitale, $address, $postcode, $city, $practical, $num_tel, $premium)
+{  
+        $data = array (
+            'password'  => $pwd,
+            'name'  => $name,
+            'surname'  => $surname,
+            'gender'  => $gender,
+            'bday'  => $bday,
+            'job'  => $job,
+            'speciality'  => $job,
+            'experience'  => $exp,
+            'num_RPPS'  => $num_RPPS,
+            'AGA'  => $aga,
+            'payment'  => $payment,
+            'carte_vitale'  => $carte_vitale,
+            'address'  => $address,
+            'postal_code'  => $postcode,
+            'city'  => $city,
+            'practical_info'  => $practical,
+            'num_tel'  => $num_tel,
+            'premium'  => $premium,
+
+        );
+        $this->db->where('email', $email);
+        return $this->db->update('pros', $data); 
+    }
 
 }
