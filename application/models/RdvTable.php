f6e556e41069cd94074aed5b3475f1c9dd924887
Raphaël <raphaelamyco@gmail.com>
2018-04-11 16:33:33 +0200

    Ajout availibility dans db

--- application/models/RdvTable.php
+++ application/models/RdvTable.php
@@ -8,16 +8,14 @@ class RdvTable extends CI_Model
 	 *	Ajoute un rendez-vous
 	 *
 	 *	@param string $email_user
-     *	@param string $email_pro
      *	@param string $date
      *	@param string $time
      *	@param string $cause
 	 *	@return bool Le résultat de la requête
 	 */
-	public function add_rdv($email_user, $email_pro, $date, $time, $cause)
+	public function add_rdv($email_user, $date, $time, $cause)
 	{
 		return $this->db->set('email_user', $email_user)
-                        ->set('email_pro', $email_pro)
                         ->set('date', $date)
                         ->set('time', $time)
                         ->set('cause', $cause)
