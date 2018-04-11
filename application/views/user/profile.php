39c558370819bc3fd97774fda655e07674c4f77b
LAPTOP-VPKFPQAM\camil <camille.zordan@hotmail.fr>
2018-04-11 09:35:33 +0200

    Modif liens profile user

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -17,14 +17,14 @@
 <body>
 
     <div id="container2_user">
-        <h1>Mon espace personnel "......"</h1>
+        <h1>Mon espace personnel</h1>
 
         <div class="navbar_user">
             <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-secondary"><a href="profil_user.php"><FONT COLOR =#FFFFFF> Mes coordonnées</FONT></a></button>
-                <button type="button" class="btn btn-secondary"><a href="avis_user.php"><FONT COLOR =#FFFFFF> Mes avis</FONT></a></button>
-                <button type="button" class="btn btn-secondary"><a href="rdv_user.php"><FONT COLOR =#FFFFFF> Mes rendez-vous</FONT></a></button>
-                <button type="button" class="btn btn-secondary"><a href="liste_medecin.php"><FONT COLOR =#FFFFFF>Mes praticiens</FONT></a></button>
+                <button type="button" class="btn btn-secondary"><a href="<?php echo site_url("user/profile"); ?>"><FONT COLOR =#FFFFFF> Mes coordonnées</FONT></a></button>
+                <button type="button" class="btn btn-secondary"><a href="<?php echo site_url("user/avis"); ?>"><FONT COLOR =#FFFFFF> Mes avis</FONT></a></button>
+                <button type="button" class="btn btn-secondary"><a href="<?php echo site_url("user/rdv"); ?>"><FONT COLOR =#FFFFFF> Mes rendez-vous</FONT></a></button>
+                <button type="button" class="btn btn-secondary"><a href="<?php echo site_url("user/liste_medecin"); ?>"><FONT COLOR =#FFFFFF>Mes praticiens</FONT></a></button>
             </div>
         </div>
 
@@ -47,6 +47,40 @@
         </div>
     </div>
 </div>
+
+<footer>
+        <div id="containerFin">
+        <img class="logo-fin" src="<?php echo base_url('/assets/images/website/Logo-03.png'); ?>" width=200px>
+        <table class="table">
+            <thead>
+                <tr>
+                    <!--coucou -->
+                    <th scope="col">MedFocus</th>
+                    <th scope="col">CGU</th>
+                    <th scope="col">Professionnel de santé ?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/about"); ?>">A propos de nous<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/faq"); ?>">Questions fréquentes<br></a>
+                        <a class =" lien-normal text-blanc" href="<?php echo site_url("home/recrutement"); ?>">Recrutement<br></a>
+                        <a class =" lien-normal text-blanc" href="<?php echo site_url("home/contact"); ?>">Contact</a>
+                    </td>
+                    <td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/cgu"); ?>">Conditions générales d'utilisation<br></a>
+                    </td> 
+                    <td>
+                        <a class =" lien-normal text-blanc text-blanc" href ="<?php echo site_url("home/services"); ?>">Nos services<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/tarifs"); ?>">Nos tarifs<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/eds"); ?>">Etablissements de santé<br></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </footer>
 
 </body>
 </html>
