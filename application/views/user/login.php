69781bd855def8d28f3aa0a846a6304adfe36b02
LAPTOP-VPKFPQAM\camil <camille.zordan@hotmail.fr>
2018-04-12 17:46:52 +0200

    login user+med

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -28,34 +28,73 @@
     	</header>
 	</div>
 
-    <div id="container2_user">
-        <h1>Connectez vous à votre espace personnel</h1>
-        <form id="form" method="post" action="">
+        <h1 class="center">Connectez-vous à votre espace personnel</h1>
+        <div class="row">
+        	<div class="col">
+        	</div>
+        	<div class="col">
+        		<form id="form" method="post" action="">
 
-			<div class="form-group">
-				<label for="email"> Adresse e-mail </label>
-				<input type="email" name="email" value="<?php echo set_value('email'); ?>" class="form-control" aria-describedby="emailHelp" placeholder="Email" />
-				<?php echo form_error('email'); ?>
-			</div>
+					<div class="form-group">
+						<label for="email"> Adresse e-mail </label>
+						<input type="email" name="email" value="<?php echo set_value('email'); ?>" class="form-control" aria-describedby="emailHelp" placeholder="Email" />
+						<?php echo form_error('email'); ?>
+					</div>
 
-			<div class="form-group">
-				<label for="pwd"> Mot de passe </label>
-				<input type="password" name="pwd" value="<?php echo set_value('pwd'); ?>" class="form-control" placeholder="Password"/>
-				<?php echo form_error('pwd'); ?>
-			</div>
+					<div class="form-group">
+						<label for="pwd"> Mot de passe </label>
+						<input type="password" name="pwd" value="<?php echo set_value('pwd'); ?>" class="form-control" placeholder="Password"/>
+						<?php echo form_error('pwd'); ?>
+					</div>
 
-			<article class="col-md-8">
-			<?php if ($error) : ?>
-				<div class="panel panel-danger">
-					<div class="panel-heading">E-mail ou mot de passe incorrect</div>
-				</div>
-			<?php endif; ?>
-			</article>
+					<article class="col-md-8">
+					<?php if ($error) : ?>
+						<div class="panel panel-danger">
+							<div class="panel-heading">E-mail ou mot de passe incorrect</div>
+						</div>
+					<?php endif; ?>
+					</article>
 
-            <button type="submit" value="Envoyer" class="btn btn-info"> Accéder à mon compte </button>
-            <p><a href="<?php echo site_url("user/signup"); ?>" class="text-info">Pas encore inscrit ?</a></p>
-    	</form>
-	</div>
+		            <button type="submit" value="Envoyer" class="btn btn-info"> Accéder à mon compte </button>
+		            <p><a href="<?php echo site_url("user/signup"); ?>" class="text-info">Pas encore inscrit ?</a></p>
+		    	</form>
+	        </div>
+	        <div class="col">
+	        </div>
+	    </div>
+	<footer>
+        <div id="containerFin">
+        <img class="logo-fin" src="<?php echo base_url('/assets/images/website/Logo-03.png'); ?>" width=200px>
+        <table class="table">
+            <thead>
+                <tr>
+                    <!--coucou -->
+                    <th scope="col">MedFocus</th>
+                    <th scope="col">CGU</th>
+                    <th scope="col">Professionnel de santé ?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/about"); ?>">A propos de nous<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/faq"); ?>">Questions fréquentes<br></a>
+                        <a class =" lien-normal text-blanc" href="<?php echo site_url("home/recrutement"); ?>">Recrutement<br></a>
+                        <a class =" lien-normal text-blanc" href="<?php echo site_url("home/contact"); ?>">Contact</a>
+                    </td>
+                    <td>
+                        <a class =" lien-normal text-blanc" href = "<?php echo site_url("home/cgu"); ?>">Conditions générales d'utilisation<br></a>
+                    </td> 
+                    <td>
+                        <a class =" lien-normal text-blanc text-blanc" href ="<?php echo site_url("home/services"); ?>">Nos services<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/tarifs"); ?>">Nos tarifs<br></a>
+                        <a class =" lien-normal text-blanc" href ="<?php echo site_url("home/eds"); ?>">Etablissements de santé<br></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </footer>
 
 </body>
 </html>
